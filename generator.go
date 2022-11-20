f2bbace8ad1c670fdce0864891fdff7cfb333f52
George Burgyan <gburgyan@vec.com>
2022-11-20 06:40:17 -0600

    Added doc comment.

--- generator.go
+++ generator.go
@@ -146,6 +146,9 @@ func (d *DependencyContext) getGeneratorOutputSlots(activeSlot *slot) []*slot {
 	return result
 }
 
+// validateGeneratorForSlot verifies that the generator's dependencies can nominally be
+// fulfilled by the dependencies present. This does not check for cyclic dependencies as
+// that would be more expensive.
 func (d *DependencyContext) validateGeneratorForSlot(s *slot) bool {
 	if s.value != nil {
 		return true
