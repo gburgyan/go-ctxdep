2895b27af0b0e5c5b3ef2d72e22d6fa80af48ced
George Burgyan <gburgyan@vec.com>
2022-11-21 07:13:38 -0600

    Add additional benchmarks.

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,6 +2,7 @@ package ctxdep
 
 import (
 	"context"
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +21,54 @@ func BenchmarkGetInterface(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		_ = Get[testInterface](ctx)
+	}
+}
+
+func BenchmarkGetSimpleGenerator(b *testing.B) {
+	ctx := NewDependencyContext(context.Background(), func() *testWidget {
+		return &testWidget{val: 42}
+	})
+	dc := GetDependencyContext(ctx)
+	t := reflect.TypeOf(&testWidget{})
+	s := dc.slots[t]
+
+	for i := 0; i < b.N; i++ {
+		_ = Get[*testWidget](ctx)
+		// Intentionally clear the generated value using a non-public value.
+		s.value = nil
+	}
+}
+
+func BenchmarkGetGeneratorWithDepenency(b *testing.B) {
+	ctx := NewDependencyContext(context.Background(), func() *testWidget {
+		return &testWidget{val: 42}
+	}, func(_ *testWidget) *testDoodad {
+		return &testDoodad{val: "105"}
+	})
+	dc := GetDependencyContext(ctx)
+	t := reflect.TypeOf(&testDoodad{})
+	s := dc.slots[t]
+
+	for i := 0; i < b.N; i++ {
+		_ = Get[*testDoodad](ctx)
+		// Intentionally clear the generated value using a non-public value.
+		s.value = nil
+	}
+}
+
+func BenchmarkGetGeneratorWithDepenencyAndContext(b *testing.B) {
+	ctx := NewDependencyContext(context.Background(), func() *testWidget {
+		return &testWidget{val: 42}
+	}, func(_ context.Context, _ *testWidget) *testDoodad {
+		return &testDoodad{val: "105"}
+	})
+	dc := GetDependencyContext(ctx)
+	t := reflect.TypeOf(&testDoodad{})
+	s := dc.slots[t]
+
+	for i := 0; i < b.N; i++ {
+		_ = Get[*testDoodad](ctx)
+		// Intentionally clear the generated value using a non-public value.
+		s.value = nil
 	}
 }
